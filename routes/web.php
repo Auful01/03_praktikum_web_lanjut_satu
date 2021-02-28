742ae2f05737fabbfd942a213bfe435272f79534
auful01 <auful@gmail.com>
2021-02-28 17:36:54 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContacController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProgramController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\WelcomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +19,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/home', [HomeController::class,'index']);
+
+Route::prefix('game')->group(function(){
+    Route::get('/list-game', [ProductController::class,'product']);
 });
+
+Route::get('news', function () {
+    return view('news',[NewsController::class, 'news' => 'news','topic' => 'educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19']);
+});
+
+Route::prefix('program')->group(function(){
+    Route::get('/daftar-program', [ProgramController::class,'program']);
+});
+
+Route::get('/about-us', [AboutController::class,'about']);
+
+
+Route::resource('contact', ContacController::class);
+
